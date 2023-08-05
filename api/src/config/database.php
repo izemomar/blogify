1bef18a5825ecbeabd680b7603e02f05a443fc96
Omar IZEM <contact@izem.dev>
2023-08-05 02:09:50 +0100

    chore(api): add & config mongodb - add mongodb service to docker-compose - install laravel's mongodb package - add mongodb connection to the connections list - delete unused models & migrations

--- api/src/config/database.php
+++ api/src/config/database.php
@@ -62,6 +62,17 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('MONGO_DB_HOST', '127.0.0.1'),
+            'port' => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE', 'homestead'),
+            'username' => env('MONGO_DB_USERNAME', 'homestead'),
+            'password' => env('MONGO_DB_PASSWORD', 'secret'),
+            'options' => [
+                'appname' => env('APP_NAME', 'laravel'),
+            ],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
@@ -125,7 +136,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
